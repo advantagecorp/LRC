5e56ae0ee9eab97409e4d782bf93f4fd1d603c43
kobibuber <39272388+kobibuber@users.noreply.github.com>
2025-02-20 16:50:07 +0200

    Add files via upload

---
+++ AOS_DemoScript/Action.c
@@ -0,0 +1,199 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_auto_header("Accept-Language", 
+		"en-US,en;q=0.9");
+
+	web_add_cookie("userCart=%7B%22userId%22%3A-1%2C%22productsInCart%22%3A%5B%5D%7D; DOMAIN=nimbusserver.aos.com");
+
+		//lr_start_transaction("Ex7_TR00_Launch");
+
+	web_url("{URL}", 
+		"URL=http://{URL}/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/main.min.js", ENDITEM, 
+		"Url=/services.properties", ENDITEM, 
+		"Url=/css/images/closeDark.png", ENDITEM, 
+		"Url=/css/images/logo.png", ENDITEM, 
+		"Url=/css/fonts/roboto_medium_macroman/Roboto-Medium-webfont.woff", "Referer=http://{URL}/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_light_macroman/Roboto-Light-webfont.woff", "Referer=http://{URL}/css/main.min.css", ENDITEM, 
+		"Url=/css/fonts/roboto_regular_macroman/Roboto-Regular-webfont.woff", "Referer=http://{URL}/css/main.min.css", ENDITEM, 
+		"Url=/css/images/arrow_right.png", "Referer=http://{URL}/css/main.min.css", ENDITEM, 
+		"Url=/css/images/Special-offer.jpg", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=mice", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=headphones", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=tablets", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=laptops", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=speakers", ENDITEM, 
+		"Url=/css/fonts/roboto_bold_macroman/Roboto-Bold-webfont.woff", "Referer=http://{URL}/css/main.min.css", ENDITEM, 
+		"Url=/css/images/linkedin.png", ENDITEM, 
+		"Url=/css/images/facebook.png", ENDITEM, 
+		"Url=/css/images/twitter.png", ENDITEM, 
+		"Url=/css/images/GoUp.png", ENDITEM, 
+		"Url=/css/fonts/roboto_thin_macroman/Roboto-Thin-webfont.woff", "Referer=http://{URL}/css/main.min.css", ENDITEM, 
+		"Url=/css/images/Banner1.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item2.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item1.jpg", ENDITEM, 
+		"Url=/css/images/Popular-item3.jpg", ENDITEM, 
+		"Url=/css/images/Banner3.jpg", ENDITEM, 
+		"Url=/css/images/Banner2.jpg", ENDITEM, 
+		"Url=/css/images/category_banner_4.png", ENDITEM, 
+		"Url=/css/images/Filter.png", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4700", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4600", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4400", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4200", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4300", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4100", ENDITEM, 
+		"Url=/catalog/fetchImage?image_id=4500", ENDITEM, 
+		LAST);
+
+	web_url("ALL", 
+		"URL=http://{URL}/catalog/api/v1/DemoAppConfig/parameters/by_tool/ALL", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_custom_request("GetAccountConfigurationRequest", 
+		"URL=http://{URL}//accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=OPTIONS", 
+		"Resource=0", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_custom_request("GetAccountConfigurationRequest_2", 
+		"URL=http://{URL}//accountservice/ws/GetAccountConfigurationRequest", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/xml", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		"EncType=text/xml; charset=UTF-8", 
+		"Body=<?xml version=\"1.0\" encoding=\"UTF-8\"?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"><soap:Body><GetAccountConfigurationRequest xmlns=\"com.advantage.online.store.accountservice\"></GetAccountConfigurationRequest></soap:Body></soap:Envelope>", 
+		LAST);
+
+	web_url("categories", 
+		"URL=http://{URL}/catalog/api/v1/categories", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t5.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("search", 
+		"URL=http://{URL}/catalog/api/v1/deals/search?dealOfTheDay=true", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t6.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("popularProducts.json", 
+		"URL=http://{URL}/app/tempFiles/popularProducts.json", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("home-page.html", 
+		"URL=http://{URL}/app/views/home-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t8.inf", 
+		"Mode=HTML", 
+		LAST);
+		
+		//lr_end_transaction("Ex7_TR00_Launch", LR_AUTO);
+
+
+	lr_think_time(9);
+	
+	lr_start_transaction("Ex7_TR01_ClickSpeakers");
+	
+	web_reg_find("Text=SPEAKERS",
+	             LAST);
+
+	web_url("products", 
+		"URL=http://{URL}/catalog/api/v1/categories/4/products", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t9.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("attributes", 
+		"URL=http://{URL}/catalog/api/v1/categories/attributes", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("category-page.html", 
+		"URL=http://{URL}/app/views/category-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t11.inf", 
+		"Mode=HTML", 
+		LAST);
+	
+	lr_end_transaction("Ex7_TR01_ClickSpeakers", LR_AUTO);
+
+
+	lr_think_time(10);
+	
+	lr_start_transaction("Ex7_TR02_BoseSoundLinkBluetoothSpeaker");
+
+
+	web_url("all_data", 
+		"URL=http://{URL}/catalog/api/v1/categories/all_data", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t12.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("20", 
+		"URL=http://{URL}/catalog/api/v1/products/20", 
+		"Resource=0", 
+		"RecContentType=application/json", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t13.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	web_url("product-page.html", 
+		"URL=http://{URL}/app/views/product-page.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://{URL}/", 
+		"Snapshot=t14.inf", 
+		"Mode=HTML", 
+		LAST);
+	lr_end_transaction("Ex7_TR02_BoseSoundLinkBluetoothSpeaker", LR_AUTO);
+
+
+	return 0;
+}
